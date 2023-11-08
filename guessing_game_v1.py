7f7ec0016a050366ac6a52d1b0cf333a14c47d14
jabumaali <jabumaali@gmail.com>
2023-11-08 13:30:42 -0800

    Updated guessing_game_v1.py

--- guessing_game_v1.py
+++ guessing_game_v1.py
@@ -3,7 +3,7 @@ Data Analysis Techdegree
 Project 1 - A Number Guessing Game
 By: Jabr Abumaali
 Started: Nov 8, 2023
-Completed: Nov 8, 2023
+Completed: ----------
 --------------------------------
 """
 import random
@@ -12,16 +12,25 @@ import statistics
 def start_game():
     print("Welcome to the Number Guessing Game!")
 
+    # Initalize record of scores (list).
     records = []
+
+    # This outer while runs once per game, and ends when
+    # the user finally enters "n".
     while True:
+        
+        # Generate a random number, and start the
+        # number of attempts at 0.
         answer = random.randint(1,100)
         attempts = 0
-
         print("Please guess the number between 1 and 100!")
+
+        # This inner while continuously prompts the user until
+        # they find the correct answer.
         while True:
             try:
                 x = int(input())
-                if x not in range(1,100):
+                if x not in range(1,101):
                     print("Please enter a number between 1 and 100.")
                 else:
                     attempts += 1
@@ -32,21 +41,30 @@ def start_game():
                     if x == answer:
                         print("You guessed it: {}!".format(answer))
                         print("You won in {} attempts.\n".format(attempts))
+                        ### The following if statement breaks my code (no longer
+                        ### prompts for y/n, attempts continues no new numbers, etc)
+                        if attempts < min(records):
+                            print("New high score!")
                         records.append(attempts)
                         records.sort()
                         mean = round(statistics.mean(records), 2)
                         median = statistics.median(records)
                         mode = statistics.mode(records)
                         print("Mean: {}\nMedian: {}\nMode: {}\n".format(mean, median, mode))
+                        # Only break when the correct answer is reached
                         break
             except ValueError:
                 print("Please enter a number between 1 and 100.")
-            
+
+        # Wait for "y" or "n"
         while x != "y" and x != "n":
             x = input("Would you like to play again? y/n: ")
+
+        # Only end the game when "n" is chosen.
         if x == "n":
             break
-        
+
+        # Show 
         print("High score: {}".format(min(records)))
         
     print("Thanks for playing!")
