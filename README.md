1b99962a38f4b1fa0d8461e579a80bfef5d5df03
jabumaali <jabumaali@gmail.com>
2023-11-08 12:06:04 -0800

    Implement all project objectives.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# First Techdegree EXAMPLE Project
-  This is just to practice uploading repositories to GitHub.
+	First Techdegree Project
+ 	This is my first TeamTreehouse Techdegree Project
+
+	This project is a number guessing game, coded in python3.
+	A random number between 1 and 100 is generated, and the user tries to guess it in as few tries as possible.
+	
+	This project includes some python basics such as try/except error handling, random, input/output, and some loop control.
--- guessing_game_v1.py
+++ guessing_game_v1.py
@@ -1,33 +1,55 @@
 """
 Data Analysis Techdegree
 Project 1 - A Number Guessing Game
+By: Jabr Abumaali
+Started: Nov 8, 2023
+Completed: Nov 8, 2023
 --------------------------------
 """
+import random
+import statistics
 
-# Import the random and statistics modules.
+def start_game():
+    print("Welcome to the Number Guessing Game!")
 
-# Create the start_game function.
-# Write your code inside this function.
+    records = []
+    while True:
+        answer = random.randint(1,100)
+        attempts = 0
 
-#   When the program starts, we want to:
-#   ------------------------------------
-#   1. Display an intro/welcome message to the player.
-#   2. Store a random number as the answer/solution.
-#   3. Continuously prompt the player for a guess.
-#     a. If the guess is greater than the solution, display to the player "It's lower".
-#     b. If the guess is less than the solution, display to the player "It's higher".
+        print("Please guess the number between 1 and 100!")
+        while True:
+            try:
+                x = int(input())
+                if x not in range(1,100):
+                    print("Please enter a number between 1 and 100.")
+                else:
+                    attempts += 1
+                    if x < answer:
+                        print("Too low.")
+                    if x > answer:
+                        print("Too high.")
+                    if x == answer:
+                        print("You guessed it: {}!".format(answer))
+                        print("You won in {} attempts.\n".format(attempts))
+                        records.append(attempts)
+                        records.sort()
+                        mean = round(statistics.mean(records), 2)
+                        median = statistics.median(records)
+                        mode = statistics.mode(records)
+                        print("Mean: {}\nMedian: {}\nMode: {}\n".format(mean, median, mode))
+                        break
+            except ValueError:
+                print("Please enter a number between 1 and 100.")
+            
+        while x != "y" and x != "n":
+            x = input("Would you like to play again? y/n: ")
+        if x == "n":
+            break
+        
+        print("High score: {}".format(min(records)))
+        
+    print("Thanks for playing!")
 
-#   4. Once the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a list.
-#   5. Display the following data to the player
-#     a. How many attempts it took them to get the correct number in this game
-#     b. The mean of the saved attempts list
-#     c. The median of the saved attempts list
-#     d. The mode of the saved attempts list
-#   6. Prompt the player to play again
-#     a. If they decide to play again, start the game loop over.
-#     b. If they decide to quit, show them a goodbye message.
 
-# ( You can add more features/enhancements if you'd like to. )
-
-
-# Kick off the program by calling the start_game function.
+start_game()
